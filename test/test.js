24e0da0274274aa3880654e1f320976bd27a4a4f
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-11-05 22:15:23 -0800

    tests pass

--- test/test.js
+++ test/test.js
@@ -2,8 +2,7 @@ var expect = require('chai').expect;
 var push = require(__dirname + '/../lib/push');
 var pop = require(__dirname + '/../lib/pop');
 var shift = require(__dirname + '/../lib/shift');
-
-
+var unshift = require(__dirname + '/../lib/unshift');
 
 describe('push function', function() {
   var testArray = [1, 2, 3];
@@ -18,10 +17,31 @@ describe('push function', function() {
 
 describe('pop function', function() {
   var testArray = [1, 2, 3];
-  it('should return the first number', function() {
+  it('should return the last number', function() {
     expect(pop(testArray)).to.eql(3);
   });
   it('should shorten the given array', function() {
     expect(testArray.length).to.eql(2);
+  });
+});
+
+describe('shift function', function() {
+  var testArray = [1, 2, 3];
+  it('should return the first number', function() {
+    expect(shift(testArray)).to.eql(1);
+  });
+  it('should shorten the given array', function() {
+    expect(testArray.length).to.eql(2);
+  });
+});
+
+describe('unshift function', function() {
+  var testArray = [1, 2, 3];
+  unshift(testArray, 4);
+  it('should add an item to an array', function() {
+    expect(testArray).to.have.length(4);
+  });
+  it('added element should be the first one', function() {
+    expect(testArray[0]).to.eql(4);
   });
 });
